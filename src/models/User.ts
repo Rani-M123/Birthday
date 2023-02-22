73ed6dd239cfee9797715ef12987e0d1492676e7
M.Rani <M.Rani>
2023-02-22 18:53:58 +0530

    first commit

---
+++ src/models/User.ts
@@ -0,0 +1,9 @@
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  month: string;
+  monthIndex: number;
+  date: any;
+  BirthdayDate: any;
+}   
---
+++ src/webparts/birthdaysPerMonth/components/BirthdaysPerMonth.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { BirthdaysInMonth } from "../../../models/BirthdaysInMonth";
+import { MonthSection } from "./MonthSection";
+
+interface IBirthdaysPerMonthProps {
+  data: Array<BirthdaysInMonth>;
+}
+
+const BirthdaysPerMonth = (props: IBirthdaysPerMonthProps): JSX.Element => {
+
+  return (
+    <section>
+      {props.data.map((month:any, index) => (
+<MonthSection key={month.title} data={month} index={index} />
+        
+      ))}
+    </section>
+  );
+};
+
+export { IBirthdaysPerMonthProps, BirthdaysPerMonth };
